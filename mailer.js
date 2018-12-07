8ede5545fd80e9b08618dd5e700fe1ecc3bfb283
namdaoduy <lenkc98@gmail.com>
2018-12-08 01:13:49 +0700

    Add Accept guest, send ticket email

--- mailer.js
+++ mailer.js
@@ -1,34 +1,56 @@
 const nodemailer = require('nodemailer')
 
-const USER = 'dev.namdaoduy@gmail.com';
-const PASS = process.env.EE_MAIL_PASS || "";
-
 class _Mailer {
   constructor() {
+    this.USER = 'dev.namdaoduy@gmail.com';
+    this.PASS = process.env.EE_MAIL_PASS || "";
     this.transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: USER,
-        pass: PASS
+        user: this.USER,
+        pass: this.PASS
       }
     });
   }
 
   sendVerifyEmail(user, link) {
     let mailOptions = {
-      from: `Easy Event <${USER}>`,
+      from: `Easy Event <${this.USER}>`,
       to: user.email,
       subject: 'Xác nhận email đăng ký tham gia sự kiện',
       text: `Easy Event chào bạn! Hãy truy cập link sau để xác nhận email đăng ký tham gia sự kiện: ${link}`,
       html: `<b>Easy Event chào ${user.name}!</b><br/>
       Hãy truy cập link sau để xác nhận email đăng ký tham gia sự kiện: <br/>
       <a href="${link}">${link}</a>`
-    };
+    }
 
     this.transporter.sendMail(mailOptions, (error, info) => {
       if (error) return console.log(error)
       console.log(info);
-    });
+    })
+  }
+
+  sendTicketEmail(user, dataImage) {
+    console.log(dataImage)
+    let mailOptions = {
+      from: `Easy Event <${this.USER}>`,
+      to: user.email,
+      subject: 'Vé tham gia sự kiện từ Easy Event',
+      text: `Easy Event chào bạn! Chúc mừng bạn đã đăng ký thành công tham gia sự kiện! Nếu không thấy dữ liệu vé, vui lòng xem email trên PC hoặc smartphone`,
+      html: `<b>Easy Event chào ${user.name}!</b><br/>
+      Chúc mừng bạn đã đăng ký thành công tham gia sự kiện! <br/>
+      Đây là thông tin vé của bạn trong đính kèm <br/>
+      <img src="cid:${this.USER}"/>`,
+      attachments:[{
+        filename: "ticket.png",
+        href: dataImage
+      }],
+    }
+
+    this.transporter.sendMail(mailOptions, (error, info) => {
+      if (error) return console.log(error)
+      console.log(info);
+    })
   }
 }
 
